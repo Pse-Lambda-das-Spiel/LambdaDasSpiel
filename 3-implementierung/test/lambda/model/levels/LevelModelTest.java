6f89a9cec7049aae38dc4262ea446ab1d56cb688
kay <schmitteckertkay@freenet.de>
2015-01-23 14:52:08 +0100

    updated test

--- 3-implementierung/test/lambda/model/levels/LevelModelTest.java
+++ 3-implementierung/test/lambda/model/levels/LevelModelTest.java
@@ -1,7 +1,9 @@
-package test.lambda.model.levels;
+package lambda.model.levels;
 
 import lambda.model.levels.ElementType;
 import lambda.model.levels.LevelModel;
+import lambda.model.levels.ReductionStrategy;
+import lambda.model.levels.TutorialMessage;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
@@ -9,6 +11,8 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
+import java.util.LinkedList;
 
 
 /**
@@ -20,7 +24,9 @@ public class LevelModelTest {
 
     @Before
     public void setUp() throws Exception {
-        level = new LevelModel();
+        level = new LevelModel(13, null, null, null,
+                new LinkedList<TutorialMessage>(), new LinkedList<ReductionStrategy>(),
+                new LinkedList<ElementType>(), 3, true);
 
 
     }
@@ -33,6 +39,19 @@ public class LevelModelTest {
         assertTrue(level.getUseableElements().get(1) == ElementType.PARANTHESIS);
         level.getUseableElements().add(2, ElementType.VARIABLE);
         assertTrue(level.getUseableElements().get(2) == ElementType.VARIABLE);
+
+    }
+
+    @Test
+    public void testAvailableRedStrats() {
+        level.getAvailableRedStrats().add(0, ReductionStrategy.APPLICATIVE_ORDER);
+        assertTrue(level.getAvailableRedStrats().get(0) == ReductionStrategy.APPLICATIVE_ORDER);
+        level.getAvailableRedStrats().add(1, ReductionStrategy.CALL_BY_NAME);
+        assertTrue(level.getAvailableRedStrats().get(1) == ReductionStrategy.CALL_BY_NAME);
+        level.getAvailableRedStrats().add(2, ReductionStrategy.CALL_BY_VALUE);
+        assertTrue(level.getAvailableRedStrats().get(2) == ReductionStrategy.CALL_BY_VALUE);
+        level.getAvailableRedStrats().add(3, ReductionStrategy.NORMAL_ORDER);
+        assertTrue(level.getAvailableRedStrats().get(3) == ReductionStrategy.NORMAL_ORDER);
 
     }
 
