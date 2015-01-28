0ad2f02d9872c3ac61fab710903e8fc6b6d7dc48
kay <schmitteckertkay@freenet.de>
2015-01-28 15:14:31 +0100

    added animation to VariableUIContext

--- 3-implementierung/src/lambda/viewcontroller/level/VariableUIContext.java
+++ 3-implementierung/src/lambda/viewcontroller/level/VariableUIContext.java
@@ -1,16 +1,39 @@
 package lambda.viewcontroller.level;
 
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * @author: Kay Schmitteckert
  */
-public class VariableUIContext extends ElementUIContext {
+public class VariableUIContext extends ElementUIContext implements ApplicationListener {
+
+    /**
+     * Number of columns in the animation-sheet
+     */
+    private static final int FRAME_COLS = 6;
+    /**
+     * Number of rows in the animation-sheet
+     */
+    private static final int FRAME_ROWS = 5;
 
     private Texture tVariable;
     private Animation aVariable;
+    private TextureRegion[] walkFrames;
+    private SpriteBatch spriteBatch;
+    private TextureRegion currentFrame;
+    private float stateTime;
 
     public VariableUIContext(Texture variable) {
         tVariable = variable;
@@ -21,8 +44,52 @@ public class VariableUIContext extends ElementUIContext {
      *
      * @return animation relating to the sprite
      */
-    public Animation getaVariable() {
+    public Animation getAVariable() {
         return aVariable;
     }
 
+    @Override
+    public void create() {
+        TextureRegion[][] tmp = TextureRegion.split(tVariable, tVariable.getWidth()/FRAME_COLS, tVariable.getHeight()/FRAME_ROWS);              // #10
+        walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
+        int index = 0;
+        for (int i = 0; i < FRAME_ROWS; i++) {
+            for (int j = 0; j < FRAME_COLS; j++) {
+                walkFrames[index++] = tmp[i][j];
+            }
+        }
+        aVariable = new Animation(0.025f, walkFrames);
+        spriteBatch = new SpriteBatch();
+        stateTime = 0f;
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+        stateTime += Gdx.graphics.getDeltaTime();
+        currentFrame = aVariable.getKeyFrame(stateTime, true);
+        spriteBatch.begin();
+        spriteBatch.draw(currentFrame, 50, 50);
+        spriteBatch.end();
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+        spriteBatch.dispose();
+    }
 }
