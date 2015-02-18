85c24a4ecd676157613cd63b8e968ab1fac54cf1
Farid Elhaddad <elhaddad@i09hiwi1.atis.uni-karlsruhe.de>
2015-02-18 17:07:35 +0100

    Main Menu

--- 3-implementierung/src/lambda/viewcontroller/mainmenu/MainMenuViewController.java
+++ 3-implementierung/src/lambda/viewcontroller/mainmenu/MainMenuViewController.java
@@ -4,22 +4,12 @@ import lambda.LambdaGame;
 import lambda.model.profiles.ProfileManager;
 import lambda.viewcontroller.ViewController;
 import lambda.viewcontroller.settings.SettingsViewController;
-import lambda.viewcontroller.shop.ShopViewController;
 
-import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.assets.loaders.SkinLoader;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.Pixmap.Format;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Container;
@@ -27,144 +17,146 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Slider;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
 import com.badlogic.gdx.scenes.scene2d.utils.Align;
-import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
-public class MainMenuViewController  implements Screen {	
-	 private Stage stage = new Stage();
-	    private Table table = new Table();
-	    private LambdaGame game;
-	   
-	    
-	    private Skin skin = new Skin(Gdx.files.internal("data/skins/mainMenuSkin.json"),
-	                new TextureAtlas(Gdx.files.internal("data/skins/MasterSkin.atlas")));
-	    private ImageButton backButton = new ImageButton(skin,"backButton"),
-	    settingsButton = new ImageButton(skin,"settings"),
-	     
-	    		sound_unmuted = new ImageButton(skin , "sound_unmuted"),
-	            sound_muted = new ImageButton(skin , "sound_muted");
-	    private Label coins;
-	    private Image coinBar;
-	    MainMenuViewController(LambdaGame game){
-	    	this.game = game;
-	    }
-	    @Override
-	    public void render(float delta) {
-	        Gdx.gl.glClearColor(0, 0, 0, 1);
-	        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-	        stage.act();
-	        stage.draw();
-	    }
+public class MainMenuViewController extends ViewController implements Screen {
+	private final Stage stage;
+	private final String skinJson = "data/skins/MainMenu.json";
+	
 
-	    @Override
-	    public void resize(int width, int height) {
-	    }
+	private Skin skin;
 
-	    @Override
-	    public void show() {
-	    	Container<ImageButton> backButtonContainer = new Container<ImageButton>();
-	    	backButtonContainer.pad(25);
-	    	backButtonContainer.align(Align.topLeft);
-	    	backButtonContainer.setActor(backButton);
-	        
-	        stage.addActor(backButtonContainer);
-	        backButtonContainer.setFillParent(true);
-	        
-	        
-	        
-	      Container<ImageButton> settingsButtonContainer = new Container<ImageButton>();
-	        settingsButtonContainer.pad(15);
-	        settingsButtonContainer.align(Align.bottomLeft);
-	        settingsButtonContainer.setActor(settingsButton);
-	        
-	        stage.addActor(settingsButtonContainer);
-	        settingsButtonContainer.setFillParent(true);
-	        
-	        
-	        
-	        Container<ImageButton> soundButtonContainer = new Container<ImageButton>();
-	        soundButtonContainer.pad(15);
-	        soundButtonContainer.align(Align.bottomRight);
-	        soundButtonContainer.setActor(sound_unmuted);
-	        
-	        stage.addActor(soundButtonContainer);
-	        soundButtonContainer.setFillParent(true);
-	        
-	      /*  coins = new Label(String.valueOf(ProfileManager.getManager().getCurrentProfile().getCoins()),
-	                skin);
-	        Container<Label> labelContainer = new Container();
-	        labelContainer.align(Align.topRight);
-	        labelContainer.setActor(coins);
-*/
-	        coinBar = new Image(skin, "coin_bar");
-	        Container<Image> imageContainer = new Container();
-	        imageContainer.align(Align.topRight);
-	        imageContainer.setActor(coinBar);
-	        stage.addActor(imageContainer);
-	        imageContainer.setFillParent(true);
-	    	backButton.addListener(new ClickListener() {
-            @Override
-            public void clicked(InputEvent event, float x, float y) {
-            	 Gdx.app.exit();
+	private ImageButton backButton, settingsButton ,sound_unmuted ,sound_muted ;
+	private Label coins;
+	private Image coinBar;
+	
+	/**
+     * Creates a object of the class without initializing the screen.
+     */
+	public MainMenuViewController() {
+		stage = new Stage(new ScreenViewport());
+	}
 
-            }
-        });
-	    	settingsButton.addListener(new ClickListener() {
-	            @Override
-	            public void clicked(InputEvent event, float x, float y) {
-	            	game.setScreen(new SettingsViewController());
+	@Override
+	public void render(float delta) {
+		Gdx.gl.glClearColor(0, 1, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		stage.act();
+		stage.draw();
+	}
 
-	            }
-	        });
-	        sound_unmuted.addListener(new ClickListener() {
-	            @Override
-	            public void clicked(InputEvent event, float x, float y) {
-	            	soundButtonContainer.setActor(sound_muted);
+	@Override
+	public void resize(int width, int height) {
+	}
 
-	            }
-	        });
-	    	sound_muted.addListener(new ClickListener() {
-	            @Override
-	            public void clicked(InputEvent event, float x, float y) {
-	            	soundButtonContainer.setActor(sound_unmuted);
+	@Override
+	public void show() {
 
-	            }
-	        });
-	        
-	        
+		Gdx.input.setInputProcessor(stage);
+	}
 
-	       
+	@Override
+	public void hide() {
+		dispose();
+	}
 
-	        
+	@Override
+	public void pause() {
+	}
 
-	        Gdx.input.setInputProcessor(stage);
-	    }
+	@Override
+	public void resume() {
+	}
 
-	    @Override
-	    public void hide() {
-	        dispose();
-	    }
+	@Override
+	public void dispose() {
+		stage.dispose();
+		skin.dispose();
+	}
 
-	    @Override
-	    public void pause() {
-	    }
+	@Override
+	public void queueAssets(AssetManager manager) {
+		manager.load(skinJson, Skin.class, new SkinLoader.SkinParameter(
+				"data/skins/MasterSkin.atlas"));
 
-	    @Override
-	    public void resume() {
-	    }
+	}
 
-	    @Override
-	    public void dispose() {
-	        stage.dispose();
-	        skin.dispose();
-	    }
+	@Override
+	public void create(AssetManager manager) {
+		skin = manager.get(skinJson, Skin.class);
+		backButton = new ImageButton(skin, "backButton");
+		settingsButton = new ImageButton(skin, "settings");
+		sound_unmuted = new ImageButton(skin, "sound_unmuted");
+		sound_muted = new ImageButton(skin, "sound_muted");
 
-		
+		Container<ImageButton> settingsButtonContainer = new Container<ImageButton>();
+		settingsButtonContainer.pad(15);
+		settingsButtonContainer.align(Align.bottomLeft);
+		settingsButtonContainer.setActor(settingsButton);
+
+		stage.addActor(settingsButtonContainer);
+		settingsButtonContainer.setFillParent(true);
+
+		Container<ImageButton> soundButtonContainer = new Container<ImageButton>();
+		soundButtonContainer.pad(15);
+		soundButtonContainer.align(Align.bottomRight);
+		soundButtonContainer.setActor(sound_unmuted);
+
+		stage.addActor(soundButtonContainer);
+		soundButtonContainer.setFillParent(true);
+
+		coins = new Label(String.valueOf(ProfileManager.getManager()
+				.getCurrentProfile().getCoins()), skin);
+		Container<Label> labelContainer = new Container<Label>();
+		labelContainer.align(Align.topRight);
+		labelContainer.setActor(coins);
+
+		coinBar = new Image(skin, "coin_bar");
+		Container<Image> imageContainer = new Container<Image>();
+		imageContainer.align(Align.topRight);
+		imageContainer.setActor(coinBar);
+		stage.addActor(imageContainer);
+		imageContainer.setFillParent(true);
+		Container<ImageButton> backButtonContainer = new Container<ImageButton>();
+		backButtonContainer.pad(25);
+		backButtonContainer.align(Align.topLeft);
+		backButtonContainer.setActor(backButton);
+
+		stage.addActor(backButtonContainer);
+		backButtonContainer.setFillParent(true);
+
+		backButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				Gdx.app.exit();
+
+			}
+		});
+		settingsButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				getGame().setScreen(SettingsViewController.class);
+				
+
+			}
+		});
+		sound_unmuted.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				soundButtonContainer.setActor(sound_muted);
+
+			}
+		});
+		sound_muted.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				soundButtonContainer.setActor(sound_unmuted);
+
+			}
+		});
+
+	}
 
 }
