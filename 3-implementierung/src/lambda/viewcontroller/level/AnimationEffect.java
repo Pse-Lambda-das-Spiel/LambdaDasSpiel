c683b7080a5a13b38c4f8d06743adb7874058803
kay <schmitteckertkay@freenet.de>
2015-02-25 23:45:29 +0100

    new animation class

--- 3-implementierung/src/lambda/viewcontroller/level/AnimationEffect.java
+++ 3-implementierung/src/lambda/viewcontroller/level/AnimationEffect.java
@@ -2,52 +2,43 @@ package lambda.viewcontroller.level;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import lambda.viewcontroller.ViewController;
 
 /**
  * @author: Kay Schmitteckert
  */
-public class MagicAnimation implements ApplicationListener {
+public class AnimationEffect implements ApplicationListener {
 
     private Animation animation;
     private SpriteBatch spriteBatch;
     private TextureAtlas atlas;
     private float stateTime;
-
-    private AssetManager assets;
-    private final String magicAtlas = "data/animation/magic/Magic_Animation.atlas";
-
-    public MagicAnimation() {
-    }
+    private int x;
+    private int y;
 
     /**
-     * Returns an animation relating to the sprite
-     *
-     * @return animation relating to the sprite
+     * Constructor of this class
+     * 
+     * @param atlas TextureAtlas which contains every single frame of the animation
      */
-    public Animation getAVariable() {
-        return animation;
+    public AnimationEffect(TextureAtlas atlas, int positionX, int positionY) {
+        this.atlas = atlas;
+        x = positionX;
+        y = positionY;
     }
 
     @Override
     public void create() {
         spriteBatch = new SpriteBatch();
-        atlas = new TextureAtlas(Gdx.files.internal(magicAtlas));
         animation = new Animation(1/10f, atlas.getRegions());
-        
     }
 
 
     @Override
     public void resize(int i, int i1) {
-
     }
 
     @Override
@@ -55,7 +46,7 @@ public class MagicAnimation implements ApplicationListener {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
         spriteBatch.begin();
         stateTime += Gdx.graphics.getDeltaTime();
-        spriteBatch.draw(animation.getKeyFrame(stateTime, true), 300, 300);
+        spriteBatch.draw(animation.getKeyFrame(stateTime, true), x, y);
         spriteBatch.end();
     }
 
@@ -69,10 +60,8 @@ public class MagicAnimation implements ApplicationListener {
 
     }
 
-
     @Override
     public void dispose() {
         spriteBatch.dispose();
-        atlas.dispose();
     }
 }
