5c4f5c07f0e6a8202a244c9df40bdaa0987c4f88
Robert Hochweiss <robert.hochweiss@yahoo.de>
2015-03-26 10:25:42 +0100

    Small fix

--- 4-qualitätssicherung/Project/pse_lambda_submission/tests/test/lambda/model/lambdaterm/LambdaTermVisitorTest.java
+++ 4-qualitätssicherung/Project/pse_lambda_submission/tests/test/lambda/model/lambdaterm/LambdaTermVisitorTest.java
@@ -1,31 +1,48 @@
 package lambda.model.lambdaterm;
 
 import java.text.ParseException;
+import java.util.HashSet;
+import java.util.Set;
+
 import lambda.model.lambdaterm.visitor.CopyVisitor;
 import lambda.model.lambdaterm.visitor.FrontInserter;
 import lambda.model.lambdaterm.visitor.IsAlphaEquivalentVisitor;
 import lambda.model.lambdaterm.visitor.SiblingInserter;
 import lambda.model.lambdaterm.visitor.ToStringVisitor;
+import lambda.model.lambdaterm.visitor.strategy.BetaReductionVisitor;
 import lambda.model.lambdaterm.visitor.strategy.ReductionStrategyApplicativeOrder;
 import lambda.model.lambdaterm.visitor.strategy.ReductionStrategyCallByName;
 import lambda.model.lambdaterm.visitor.strategy.ReductionStrategyCallByValue;
 import lambda.model.lambdaterm.visitor.strategy.ReductionStrategyNormalOrder;
+import lambda.model.levels.LevelManager;
+
 import org.junit.After;
 import org.junit.AfterClass;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.badlogic.gdx.graphics.Color;
+import com.libgdxtesting.GdxTestRunner;
 
 /**
  * Tests lambda term visitors.
  * 
  * @author Florian Fervers
  */
+@RunWith(GdxTestRunner.class)
 public class LambdaTermVisitorTest {
+	static Set<Color> alphaConversionColors;
+	
     @BeforeClass
     public static void setUpClass() {
+    	LevelManager.getLevelManager();
+    	alphaConversionColors = new HashSet<>(LevelManager.getAllColors());
     }
     
     @AfterClass
@@ -85,15 +102,15 @@ public class LambdaTermVisitorTest {
         assertTrue("IsAlphaEquivalentVisitor returns false for equal terms!", term1.accept(new IsAlphaEquivalentVisitor(term2)));
         
         term1 = LambdaUtils.fromString("(/x.x /y.y x) y");
-        term2 = LambdaUtils.fromString("(/a.a /z.z a) y");
+        term2 = LambdaUtils.fromString("(/v.v /z.z v) y");
         assertTrue("IsAlphaEquivalentVisitor returns false for alpha equivalent terms!", term1.accept(new IsAlphaEquivalentVisitor(term2)));
         
         term1 = LambdaUtils.fromString("(/x.x /y.y x) y");
-        term2 = LambdaUtils.fromString("(/a.a /z.z a) z");
+        term2 = LambdaUtils.fromString("(/v.v /z.z v) z");
         assertTrue("IsAlphaEquivalentVisitor returns true for terms with different free variables!", !term1.accept(new IsAlphaEquivalentVisitor(term2)));
         
         term1 = LambdaUtils.fromString("(/x.x /y.y x) y");
-        term2 = LambdaUtils.fromString("(/a.a /z.z b) v");
+        term2 = LambdaUtils.fromString("(/t.t /z.z s) v");
         assertTrue("IsAlphaEquivalentVisitor returns true for terms where bound variable is not correctly translated!", !term1.accept(new IsAlphaEquivalentVisitor(term2)));
     }
     
@@ -105,8 +122,10 @@ public class LambdaTermVisitorTest {
     @Test
     public void testAlphaConversionInBetaReduction() throws ParseException {
         LambdaTerm term = LambdaUtils.fromString("(/x.x /y.y x) y");
-        term.accept(new ReductionStrategyNormalOrder());
-        LambdaTerm reduced = LambdaUtils.fromString("y /g.g y");
+        BetaReductionVisitor normalOrder = new ReductionStrategyNormalOrder();
+        normalOrder.setAlphaConversionColors(alphaConversionColors);
+        term.accept(normalOrder);
+        LambdaTerm reduced = LambdaUtils.fromString("y /v.v y");
 
         assertTrue("No alpha conversion performed in beta reduction where free variable should stay free!", term.accept(new IsAlphaEquivalentVisitor(reduced)));
     }
@@ -119,10 +138,13 @@ public class LambdaTermVisitorTest {
     @Test
     public void testBetaReductionNormalStrategy() throws ParseException {
         LambdaTerm term = LambdaUtils.fromString("(/z.z z) (/z.z z)"); // Non-terminating term
-        assertEquals("Term that beta-reduces to itself is modified by ReductionStrategyNormalOrder!", term, term.accept(new CopyVisitor()).accept(new ReductionStrategyNormalOrder()));
+        BetaReductionVisitor normalOrder = new ReductionStrategyNormalOrder();
+        normalOrder.setAlphaConversionColors(alphaConversionColors);
+        assertEquals("Term that beta-reduces to itself is modified by ReductionStrategyNormalOrder!", term, term.accept(new CopyVisitor()).accept(normalOrder));
+       
         
         term = LambdaUtils.fromString("(/x.y) ((/z.z z) (/z.z z))"); // Terminating in normal order
-        assertEquals("Normal order beta reduction not correct!", LambdaUtils.fromString("y"), term.accept(new CopyVisitor()).accept(new ReductionStrategyNormalOrder()));
+        assertEquals("Normal order beta reduction not correct!", LambdaUtils.fromString("y"), term.accept(new CopyVisitor()).accept(normalOrder));
     }
     
     /**
@@ -132,11 +154,13 @@ public class LambdaTermVisitorTest {
      */
     @Test
     public void testBetaReductionApplicativeStrategy() throws ParseException {
+    	 BetaReductionVisitor applicativeOrder = new ReductionStrategyApplicativeOrder();
+         applicativeOrder.setAlphaConversionColors(alphaConversionColors);
         LambdaTerm term = LambdaUtils.fromString("(/z.z z) (/z.z z)"); // Non-terminating term
-        assertEquals("Term that beta-reduces to itself is modified by ReductionStrategyApplicativeOrder!", term, term.accept(new CopyVisitor()).accept(new ReductionStrategyApplicativeOrder()));
-    
+        assertEquals("Term that beta-reduces to itself is modified by ReductionStrategyApplicativeOrder!", term, term.accept(new CopyVisitor()).accept(applicativeOrder));
+        
         term = LambdaUtils.fromString("(/x.y) ((/z.z z) (/z.z z))"); // Non-terminating in applicative order
-        assertEquals("Applicative order beta reduction not correct!", term, term.accept(new CopyVisitor()).accept(new ReductionStrategyApplicativeOrder()));
+        assertEquals("Applicative order beta reduction not correct!", term, term.accept(new CopyVisitor()).accept(applicativeOrder));
     }
     
     /**
@@ -147,18 +171,20 @@ public class LambdaTermVisitorTest {
     @Test
     public void testBetaReductionCallByValueStrategy() throws ParseException {
         LambdaTerm term = LambdaUtils.fromString("(/x.x) (y y)");
-        assertEquals("Term should not be reduced by call-by-value strategy!", term, term.accept(new CopyVisitor()).accept(new ReductionStrategyCallByValue()));
+        BetaReductionVisitor callByValue = new ReductionStrategyCallByValue();
+       callByValue.setAlphaConversionColors(alphaConversionColors);
+        assertEquals("Term should not be reduced by call-by-value strategy!", term, term.accept(new CopyVisitor()).accept(callByValue));
         
         term = LambdaUtils.fromString("/x./y.y x");
-        assertEquals("Term should not be reduced by call-by-value strategy!", term, term.accept(new CopyVisitor()).accept(new ReductionStrategyCallByValue()));
+        assertEquals("Term should not be reduced by call-by-value strategy!", term, term.accept(new CopyVisitor()).accept(callByValue));
     
-        term = LambdaUtils.fromString("(/a.a) ((/b.b) (/z. (/c.c) z))");
+        term = LambdaUtils.fromString("(/x.x) ((/y.y) (/z. (/v.v) z))");
+        term.accept(callByValue);
+        assertTrue("Call-by-value beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/x.x) ((/y.y) (/z. (/v.v) z))"))));
         term.accept(new ReductionStrategyCallByValue());
-        assertTrue("Call-by-value beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/a.a) ((/z. (/c.c) z))"))));
+        assertTrue("Call-by-value beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/z. (/v.v) z)"))));
         term.accept(new ReductionStrategyCallByValue());
-        assertTrue("Call-by-value beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/z. (/c.c) z)"))));
-        term.accept(new ReductionStrategyCallByValue());
-        assertTrue("Call-by-value beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/z. (/c.c) z)"))));
+        assertTrue("Call-by-value beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/z. (/v.v) z)"))));
     }
     
     /**
@@ -169,15 +195,17 @@ public class LambdaTermVisitorTest {
     @Test
     public void testBetaReductionCallByNameStrategy() throws ParseException {
         LambdaTerm term = LambdaUtils.fromString("/x./y.y x");
-        assertEquals("Term should not be reduced by call-by-name strategy!", term, term.accept(new CopyVisitor()).accept(new ReductionStrategyCallByName()));
+        BetaReductionVisitor callByName = new ReductionStrategyCallByName();
+        callByName.setAlphaConversionColors(alphaConversionColors);
+        assertEquals("Term should not be reduced by call-by-name strategy!", term, term.accept(new CopyVisitor()).accept(callByName));
     
-        term = LambdaUtils.fromString("(/a.a) ((/b.b) (/z. (/c.c) z))");
-        term.accept(new ReductionStrategyCallByName());
-        assertTrue("Call-by-name beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/a.a) ((/z. (/c.c) z))"))));
-        term.accept(new ReductionStrategyCallByName());
-        assertTrue("Call-by-name beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/z. (/c.c) z)"))));
-        term.accept(new ReductionStrategyCallByName());
-        assertTrue("Call-by-name beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/z. (/c.c) z)"))));
+        term = LambdaUtils.fromString("(/x.x) ((/v.v) (/z. (/y.y) z))");
+        term.accept(callByName);
+        assertTrue("Call-by-name beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/x.x) ((/v.v) (/z. (/y.y) z))"))));
+        term.accept(callByName);
+        assertTrue("Call-by-name beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/z. (/y.y) z)"))));
+        term.accept(callByName);
+        assertTrue("Call-by-name beta reduction not correct!", term.accept(new IsAlphaEquivalentVisitor(LambdaUtils.fromString("(/z. (/y.y) z)"))));
     }
     
     /**
